94f84342f48650af690b5c3cb3add26e4c277142
alexboisseau <alex.boisseau79@gmail.com>
2024-02-14 16:21:09 +0100

    chore: instal checkbox from shadcnui

--- core/types/http-request.ts
+++ core/types/http-request.ts
@@ -1,5 +1,15 @@
 import { HttpMethod } from "./http-method";
 
+type ActivableKeyValue = {
+  key: string;
+  value: string;
+  active: boolean;
+};
+
+export type QueryParameter = ActivableKeyValue;
+
+export type Header = ActivableKeyValue;
+
 export type HttpRequest = {
   method: HttpMethod;
   url: string;
