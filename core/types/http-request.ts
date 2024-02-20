16bff71c103f482146083266bbbf978fb7f1c91a
alexboisseau <alex.boisseau79@gmail.com>
2024-02-20 16:50:56 +0100

    feat(http-request): add body property to http request type

--- core/types/http-request.ts
+++ core/types/http-request.ts
@@ -4,6 +4,7 @@ import { HttpMethod } from "./http-method";
 export type QueryParameter = ActivatableKeyValue;
 export type Header = ActivatableKeyValue;
 export type HttpRequest = {
+  body: string | null;
   headers: Header[];
   method: HttpMethod;
   url: string;
