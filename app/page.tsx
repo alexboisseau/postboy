2ac31d83b3cf58500ade7e7ee81abcabef4bc9e4
alexboisseau <alex.boisseau79@gmail.com>
2024-02-21 17:00:29 +0100

    fix(main page styles): set min height instead of strict height to handle vertical overflow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import RequestResponse from "@/components/RequestResponse/RequestResponse";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center">
+    <main className="py-2 max-w-screen min-h-screen flex flex-col items-center justify-center">
       <RequestResponse />
     </main>
   );
