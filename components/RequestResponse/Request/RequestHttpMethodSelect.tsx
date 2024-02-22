b37150346f1bfcdc12886ea30a0f8392ad0b1c45
alexboisseau <alex.boisseau79@gmail.com>
2024-02-22 12:59:35 +0100

    feat(http methods style): add colors for http methods select

--- components/RequestResponse/Request/RequestHttpMethodSelect.tsx
+++ components/RequestResponse/Request/RequestHttpMethodSelect.tsx
@@ -10,6 +10,18 @@ import { RequestResponseActionTypes } from "../state/actions";
 import { RequestResponseContext } from "../state/context";
 import { HttpMethod } from "@/core/types/http-method";
 
+const httpMethodColors: {
+  [K in HttpMethod]: string;
+} = {
+  GET: "text-green-500",
+  POST: "text-blue-500",
+  PUT: "text-yellow-500",
+  PATCH: "text-yellow-500",
+  DELETE: "text-red-500",
+  OPTIONS: "text-gray-500",
+  HEAD: "text-gray-500",
+};
+
 export default function RequestHttpMethodSelect() {
   const {
     requestResponse: {
@@ -31,15 +43,30 @@ export default function RequestHttpMethodSelect() {
   return (
     <div className="flex flex-col gap-1">
       <Select value={httpMethod} onValueChange={handleChange}>
-        <SelectTrigger className="w-32">
+        <SelectTrigger
+          className={`w-32 font-semibold ${httpMethodColors[httpMethod]}`}
+        >
           <SelectValue placeholder="HTTP Method" />
         </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="GET">GET</SelectItem>
-          <SelectItem value="POST">POST</SelectItem>
-          <SelectItem value="PUT">PUT</SelectItem>
-          <SelectItem value="PATCH">PATCH</SelectItem>
-          <SelectItem value="DELETE">DELETE</SelectItem>
+        <SelectContent className="font-semibold">
+          <SelectItem
+            className={`${httpMethodColors.GET} hover:${httpMethodColors.GET}`}
+            value="GET"
+          >
+            GET
+          </SelectItem>
+          <SelectItem className={httpMethodColors.POST} value="POST">
+            POST
+          </SelectItem>
+          <SelectItem className={httpMethodColors.PUT} value="PUT">
+            PUT
+          </SelectItem>
+          <SelectItem className={httpMethodColors.PATCH} value="PATCH">
+            PATCH
+          </SelectItem>
+          <SelectItem className={httpMethodColors.DELETE} value="DELETE">
+            DELETE
+          </SelectItem>
         </SelectContent>
       </Select>
       {errors.httpMethod && (
