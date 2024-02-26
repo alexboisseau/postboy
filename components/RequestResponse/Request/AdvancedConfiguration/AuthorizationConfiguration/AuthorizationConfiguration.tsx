0a6afba20fb5b03b682ec160013dad6d2ead0988
alexboisseau <alex.boisseau79@gmail.com>
2024-02-26 17:43:04 +0100

    feat(request authorization): new component to select an authorization type and provide credentials

--- components/RequestResponse/Request/AdvancedConfiguration/AuthorizationConfiguration/AuthorizationConfiguration.tsx
+++ components/RequestResponse/Request/AdvancedConfiguration/AuthorizationConfiguration/AuthorizationConfiguration.tsx
@@ -1,7 +1,81 @@
-export default function AuthorizationConfiguration() {
+import {
+  AuthorizationType,
+  RequestResponseActionTypes,
+} from "@/components/RequestResponse/state/actions";
+import { RequestResponseContext } from "@/components/RequestResponse/state/context";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useContext } from "react";
+import BasicAuthorization from "./AuthorizationTypes/Basic";
+
+function AuthorizationTypeSelector() {
+  const {
+    requestResponse: {
+      request: {
+        fields: {
+          authorization: { type },
+        },
+      },
+    },
+    dispatchRequestResponseAction,
+  } = useContext(RequestResponseContext);
+
+  const handleChange = (value: AuthorizationType) => {
+    dispatchRequestResponseAction({
+      type: RequestResponseActionTypes.REQUEST_UPDATE_AUTHORIZATION_TYPE,
+      payload: value,
+    });
+  };
+
   return (
     <div>
-      <p>Authorization Configuration</p>
+      <div className="flex text-sm items-center justify-between gap-5 mb-2">
+        <span className="font-semibold">Type</span>
+        <Select value={type} onValueChange={handleChange}>
+          <SelectTrigger className={`w-32`}>
+            <SelectValue placeholder="HTTP Method" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="no-auth">No auth</SelectItem>
+            <SelectItem value="basic">Basic</SelectItem>
+            <SelectItem value="bearer">Bearer</SelectItem>
+            <SelectItem value="api-key">API Key</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <p className="text-sm">
+        The authorization header will be automatically generated when you send
+        the request.
+      </p>
+    </div>
+  );
+}
+
+export default function AuthorizationConfiguration() {
+  const {
+    requestResponse: {
+      request: {
+        fields: {
+          authorization: { type },
+        },
+      },
+    },
+  } = useContext(RequestResponseContext);
+
+  return (
+    <div className="p-2 flex gap-36">
+      <div className="w-2/6">
+        <AuthorizationTypeSelector />
+      </div>
+      <div className="w-4/6 flex items-center justify-center">
+        {type === "no-auth" && <p>No authorization</p>}
+        {type === "basic" && <BasicAuthorization />}
+      </div>
     </div>
   );
 }
