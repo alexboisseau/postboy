d593fb202c79514177afd680dbb37d4f41275250
alexboisseau <alex.boisseau79@gmail.com>
2024-03-06 12:09:56 +0100

    refactor: improve ui of sidebar

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,44 +1,48 @@
 "use client";
 import { Boxes, Container, Plus } from "lucide-react";
+import Link from "next/link";
 
-function TabLink({
-  label,
-  isActive,
-  icon,
-}: {
+type TabLinkProps = {
+  href: string;
   label: string;
   isActive: boolean;
   icon?: React.ReactNode;
-}) {
+};
+
+function TabLink({ href, label, isActive, icon }: TabLinkProps) {
   return (
-    <div
-      className={`flex gap-3 p-2 rounded items-center ${isActive ? "bg-primary-foreground border text-primary" : "text-secondary-foreground"}`}
+    <Link
+      href={href}
+      className={`flex gap-3 p-2 rounded items-center ${isActive ? "bg-primary-foreground border text-primary" : "text-zinc-700 dark:text-zinc-400"}`}
     >
       {icon !== undefined && icon}
       <span>{label}</span>
-    </div>
+    </Link>
   );
 }
 
 export default function SideBar() {
   return (
-    <div className="h-screen w-[350px] border-r-[1px]">
+    <div className="h-screen min-w-[300px] border-r-[1px]">
       <div className="h-[65px] border-b-[1px] flex flex-col justify-center">
         <h1 className="text-2xl font-bold px-8">Postboy</h1>
       </div>
       <div className="px-8 py-5 flex flex-col gap-3">
-        <p className="text-sm text-foreground">Main Menu</p>
+        <p className="text-sm text-zinc-700 dark:text-zinc-400">Main Menu</p>
         <TabLink
+          href={"/"}
           label="New Request"
           isActive={true}
           icon={<Plus size="20" />}
         />
         <TabLink
+          href={"/"}
           label="Collections (soon)"
           isActive={false}
           icon={<Boxes size="20" />}
         />
         <TabLink
+          href={"/"}
           label="Environments (soon)"
           isActive={false}
           icon={<Container size="20" />}
