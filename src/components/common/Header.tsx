eca3860f88684935a0b7ad15c7733a32f1f662f4
alexboisseau <alex.boisseau79@gmail.com>
2024-03-12 15:46:32 +0100

    feat(header): add user button component from clerk lib

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,6 +4,8 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@components/ui/button";
 import { Skeleton } from "@components/ui/skeleton";
+import { UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Header() {
   const { setTheme, theme } = useTheme();
@@ -14,29 +16,46 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="py-3 px-8 flex flex-row-reverse border-b-[1px]">
+    <header className="py-3 px-8 flex flex-row-reverse items-center gap-3 border-b-[1px]">
       {isMounted ? (
-        theme === "dark" ? (
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => setTheme("light")}
-          >
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          </Button>
-        ) : (
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => setTheme("dark")}
-          >
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          </Button>
-        )
+        <>
+          {theme === "dark" ? (
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => setTheme("light")}
+            >
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </Button>
+          ) : (
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => setTheme("dark")}
+            >
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            </Button>
+          )}
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+            }}
+            userProfileProps={{
+              appearance: {
+                baseTheme: theme === "dark" ? dark : undefined,
+              },
+            }}
+          />
+        </>
       ) : (
-        <Button variant={"ghost"} size="icon">
-          <Skeleton className="h-[1.2rem] w-[1.2rem]" />
-        </Button>
+        <>
+          <Button variant={"ghost"} size="icon" className="rounded-full">
+            <Skeleton className="h-[1.2rem] w-[1.2rem]" />
+          </Button>
+          <Button variant={"ghost"} size="icon">
+            <Skeleton className="h-[1.2rem] w-[1.2rem]" />
+          </Button>
+        </>
       )}
     </header>
   );
