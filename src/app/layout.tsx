5c3fe5bfbd523cf7acf9c5eeb9a1d51cf933aa54
alexboisseau <alex.boisseau79@gmail.com>
2024-03-12 11:02:26 +0100

    feat: add ClerkProvider to the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@components/common/Header";
 import SideBar from "@components/common/SideBar";
 import { ThemeProvider } from "@components/common/ThemeProvider";
 import StoreProvider from "@context/StoreProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,24 +21,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} w-screen max-h-screen flex`}>
-        <StoreProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            disableTransitionOnChange
-          >
-            <SideBar />
-            <div className="flex flex-col w-full items-stretch overflow-x-auto">
-              <Header />
-              <main className="h-full p-2 bg-primary-foreground overflow-y-auto">
-                {children}
-              </main>
-            </div>
-          </ThemeProvider>
-        </StoreProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} w-screen max-h-screen flex`}>
+          <StoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="light"
+              disableTransitionOnChange
+            >
+              <SideBar />
+              <div className="flex flex-col w-full items-stretch overflow-x-auto">
+                <Header />
+                <main className="h-full p-2 bg-primary-foreground overflow-y-auto">
+                  {children}
+                </main>
+              </div>
+            </ThemeProvider>
+          </StoreProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
