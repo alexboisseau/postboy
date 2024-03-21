6a74b4b39108333b1d83d8d4b499cb6e8ce5b886
alexboisseau <alex.boisseau79@gmail.com>
2024-03-21 11:27:53 +0100

    test: add unit tests for removeQueryParameter reducer

---
+++ src/context/features/currentRequest/reducers/removeQueryParameter.test.ts
@@ -0,0 +1,31 @@
+import { CurrentRequestState } from "../types";
+import createInitialState from "../utils/createInitialState";
+import removeQueryParameter from "./removeQueryParameter";
+
+const generateActionPayload = (index: number) => ({
+  payload: index,
+  type: "currentRequest/removeQueryParameter",
+});
+
+describe("removeQueryParameter", () => {
+  let state: CurrentRequestState;
+
+  beforeEach(() => {
+    state = createInitialState();
+    state.fields.queryParameters = [
+      { key: "hello", value: "alice", active: true },
+    ];
+    state.fields.url = "http://example.com/?hello=alice";
+  });
+
+  it("should remove query parameter from array", () => {
+    const updatedState = removeQueryParameter(state, generateActionPayload(0));
+
+    expect(updatedState.fields.queryParameters).toEqual([]);
+  });
+
+  it("should update the url", () => {
+    const updatedState = removeQueryParameter(state, generateActionPayload(0));
+    expect(updatedState.fields.url).toBe("http://example.com/");
+  });
+});
