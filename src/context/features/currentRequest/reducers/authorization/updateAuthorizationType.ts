946e0a35e3a59b777e53d23ace3c981609aa7939
alexboisseau <alex.boisseau79@gmail.com>
2024-03-21 17:30:38 +0100

    refactor: separate logic to generate header and store update in updateAuthorizationType reducer

--- src/context/features/currentRequest/reducers/authorization/updateAuthorizationType.ts
+++ src/context/features/currentRequest/reducers/authorization/updateAuthorizationType.ts
@@ -1,44 +1,59 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 import { AuthorizationType, CurrentRequestState } from "../../types";
+import { Header } from "@core/types/http-request";
+
+function generateAuthorizationHeaderValue(
+  state: CurrentRequestState,
+  type: AuthorizationType
+): Header {
+  switch (type) {
+    case "basic":
+      return {
+        key: "Authorization",
+        value: `Basic ${btoa(
+          `${state.fields.authorization.basic.username}:${state.fields.authorization.basic.password}`
+        )}`,
+        active: true,
+      };
+    case "bearer-token":
+      return {
+        key: "Authorization",
+        value: `Bearer ${state.fields.authorization.bearerToken.token}`,
+        active: true,
+      };
+    case "api-key":
+      return {
+        key: state.fields.authorization.apiKey.key,
+        value: state.fields.authorization.apiKey.value,
+        active: true,
+      };
+    default:
+      return {
+        key: "Authorization",
+        value: "",
+        active: true,
+      };
+  }
+}
 
 export default function updateAuthorizationType(
   state: CurrentRequestState,
   action: PayloadAction<AuthorizationType>
 ): CurrentRequestState {
   // Remove the Authorization header if it exists
-  let updatedHeaders = state.fields.headers.filter(
-    (header) => header.key !== "Authorization"
+  const updatedHeaders = state.fields.headers.filter(
+    (h) =>
+      h.key !== "Authorization" &&
+      h.key !== state.fields.authorization.apiKey.key
   );
 
-  if (state.fields.authorization.type === "api-key") {
-    updatedHeaders = state.fields.headers.filter(
-      (header) => header.key !== state.fields.authorization.apiKey.key
-    );
-  }
-
-  const headerValue = (() => {
-    if (action.payload === "basic") {
-      return `Basic ${btoa(
-        `${state.fields.authorization.basic.username}:${state.fields.authorization.basic.password}`
-      )}`;
-    } else if (action.payload === "bearer-token") {
-      return `Bearer ${state.fields.authorization.bearerToken.token}`;
-    }
-    return "";
-  })();
+  updatedHeaders.push(generateAuthorizationHeaderValue(state, action.payload));
 
   return {
     ...state,
     fields: {
       ...state.fields,
-      headers: [
-        ...updatedHeaders,
-        {
-          key: "Authorization",
-          value: headerValue,
-          active: true,
-        },
-      ],
+      headers: updatedHeaders,
       authorization: {
         ...state.fields.authorization,
         type: action.payload,
